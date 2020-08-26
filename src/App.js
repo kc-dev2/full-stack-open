71444a50a94785b45a17f642868e492afb20e535
Kyle Choi <choikyle96@gmail.com>
2020-08-26 15:48:08 -0400

    exercise 1.6

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-function App() {
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Give feedback: </h3>
+      <Button onClick={handleGood} text="Good" />
+      <Button onClick={handleBad} text="Bad"/>
+      <Button onClick={handleNeutral} text="Neutral"/>
+
+      <h3>Statistics:</h3>
+      good: {good} <br/>
+      bad: {bad} <br/>
+      neutral: {neutral} <br/>
     </div>
-  );
+  )
 }
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
 
 export default App;
