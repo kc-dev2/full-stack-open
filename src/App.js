d3999d540065a7b5339ae6edfea1692d292bf241
Kyle Choi <choikyle96@gmail.com>
2020-08-26 15:52:02 -0400

    exercise 1.7

--- src/App.js
+++ src/App.js
@@ -8,17 +8,21 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
   const handleGood = () => {
     setGood(good + 1)
+    setTotal(total + 1)
   }
 
   const handleBad = () => {
     setBad(bad + 1)
+    setTotal(total + 1)
   }
 
   const handleNeutral = () => {
     setNeutral(neutral + 1)
+    setTotal(total + 1)
   }
 
 
@@ -33,6 +37,9 @@ const App = () => {
       good: {good} <br/>
       bad: {bad} <br/>
       neutral: {neutral} <br/>
+      total: {total} <br />
+      average: {(good - bad)/total} <br />
+      positive: {good/total*100}%
     </div>
   )
 }
